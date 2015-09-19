5e01352f87855f2638eb2aa8da00acaddaaf9339
Adam Babcock <adam.babcock@smartsocialmedia.com>
2015-09-19 12:21:11 -0500

    Add basic resource list

---
+++ src/directives/directives.ts
@@ -0,0 +1,7 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+var app = angular
+    .module("incremental.directives", [
+        'incremental.generator',
+        'incremental.resourceList'
+    ]);
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+var app = angular
+    .module("incremental", [
+        'incremental.directives'
+    ]);
