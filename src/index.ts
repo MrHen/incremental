fbe66c6cf5be4b87f2ebe7031a3999b95a024cbb
Adam Babcock <adam.babcock@smartsocialmedia.com>
2015-09-19 22:35:27 -0500

    Add a controller for the app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 /// <reference path="../typings/tsd.d.ts" />
 
-var app = angular
-    .module("incremental", [
-        'incremental.directives'
-    ]);
+module IncrementalApp {
+    export interface IncrementalScope {
+        resources: {[resource:string]:number};
+    }
+
+    export class IncrementalController {
+        public scope: IncrementalScope;
+
+        public static $inject:string[] = ["$scope"];
+
+        constructor(private $scope:IncrementalScope) {
+            this.scope = $scope;
+
+            $scope.resources = {
+                'scrap': 0,
+                'junk': 0
+            };
+        }
+    }
+
+    var app = angular
+        .module("incremental", ['incremental.directives'])
+        .controller("IncrementalController", IncrementalController);
+}
