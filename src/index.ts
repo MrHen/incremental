3620889d02cf0d069584b01ff85392ded9625403
Adam Babcock <adam.babcock@smartsocialmedia.com>
2015-10-10 16:23:48 -0500

    Move converters to data driven pattern

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 /// <reference path="../typings/tsd.d.ts" />
+/// <reference path="./directives/converter/converter.ts" />
 /// <reference path="./services/dataStore/dataStore.service.ts" />
 
 namespace IncrementalApp {
     export interface IncrementalScope {
-        resources: {[resource:string]:number};
+        converters: {
+            cost:Converter.ConverterAmount[];
+            description:string;
+            gain:Converter.ConverterAmount[];
+            name:string;
+        }[];
+
+        resources: DataStore.DataSnapshot;
     }
 
     export class IncrementalController {
@@ -15,6 +23,33 @@ namespace IncrementalApp {
                 "junk": 0,
                 "scrapbot": 0
             };
+
+            this.$scope.converters = [
+                {
+                    cost: [{resource: "scrap", count: 2}],
+                    description: "-2 Scrap, +1 Junk",
+                    gain: [{resource: "junk", count: 1}],
+                    name: "convert"
+                },
+                {
+                    cost: [{resource: "junk", count: 5}],
+                    description: "-5 Junk, +1 Scrapbot",
+                    gain: [{resource: "scrapbot", count: 1}],
+                    name: "build"
+                },
+                {
+                    cost: [{resource: "scrapbot", count: 1}],
+                    description: "-1 Scrapbot, +3 Coal",
+                    gain: [{resource: "coal", count: 3}],
+                    name: "mine"
+                },
+                {
+                    cost: [{resource: "scrapbot", count: 1}, {resource: "coal", count: 5}],
+                    description: "-1 Scrapbot, -5 Coal, +30 Scrap",
+                    gain: [{resource: "scrap", count: 30}],
+                    name: "gather"
+                }
+            ];
 
             if (_.isEmpty(this.dataStore.current)) {
                 this.dataStore.current = starterKit;
